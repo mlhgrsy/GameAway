8d323f6d438f014340d6f1e3b6b8dfb40fcd5c62
Ahmet Berk TAS <ahberk.tas@gmail.com>
2021-12-06 18:50:32 +0300

    installed and configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gameaway/pages/homepage.dart';
 import 'package:gameaway/root.dart';
@@ -8,11 +9,52 @@ import 'package:gameaway/utils/styles.dart';
 import 'package:gameaway/views/product_preview.dart';
 
 void main() {
-  runApp(MaterialApp(
-    routes: {
-      '/': (context) => const Root(),
-      '/walk': (context) => const WalkThrough(),
-      '/signUp': (context) => const SignUp()
-    },
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const AppInitialization());
+}
+
+class AppInitialization extends StatefulWidget {
+  const AppInitialization({Key? key}) : super(key: key);
+
+  @override
+  _AppInitializationState createState() => _AppInitializationState();
+}
+
+class _AppInitializationState extends State<AppInitialization> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text(
+                    'No Firebase Connection: ${snapshot.error.toString()}'),
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            home: MaterialApp(
+              routes: {
+                '/': (context) => const Root(),
+                '/walk': (context) => const WalkThrough(),
+                '/signUp': (context) => const SignUp()
+              },
+            ),
+          );
+        }
+        return const MaterialApp(
+          home: Center(
+            child: Text('Connecting to Firebase'),
+          ),
+        );
+      },
+    );
+  }
 }
