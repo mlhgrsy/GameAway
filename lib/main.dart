c90fa4773dc2829758dea43e55fe80e6e616afe0
EMRE <ceski@sabanciuniv.edu>
2021-12-11 21:52:27 +0300

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gameaway/pages/homepage.dart';
 import 'package:gameaway/root.dart';
 import 'package:gameaway/pages/sign_up.dart';
 import 'package:gameaway/pages/walkthrough.dart';
+import 'package:gameaway/services/auth.dart';
 import 'package:gameaway/utils/dimensions.dart';
 import 'package:gameaway/utils/styles.dart';
 import 'package:gameaway/views/product_preview.dart';
+import 'package:provider/provider.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,12 +43,16 @@ class _AppInitializationState extends State<AppInitialization> {
           );
         }
         if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            home: const Root(),
-            routes: {
-              '/walk': (context) => const WalkThrough(),
-              '/signUp': (context) => const SignUp()
-            },
+          return StreamProvider<User?>.value(
+          value: AuthService().user,
+        initialData: null,
+            child: MaterialApp(
+              home: const Root(),
+              routes: {
+                '/walk': (context) => const WalkThrough(),
+                '/signUp': (context) => const SignUp()
+              },
+            ),
           );
         }
         return const MaterialApp(
