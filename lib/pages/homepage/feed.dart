504f4a69aeba8a7cf0afe8607827d94613c5e69f
Ahmet Berk TAS <ahberk.tas@gmail.com>
2021-12-31 13:28:48 +0300

    Combined seller names with products

--- lib/pages/homepage/feed.dart
+++ lib/pages/homepage/feed.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gameaway/services/db.dart';
 import 'package:gameaway/utils/dimensions.dart';
@@ -14,30 +15,47 @@ class Feed extends StatefulWidget {
 }
 
 class _FeedState extends State<Feed> {
+  DBService db = DBService();
+  List<Product>? _products;
+  List<Product>? _promotions;
+  List<Product>? _recommendations;
+
+  Future<void> getProducts() async {
+    var r = await db.productCollection.get();
+    var _productsTemp = r.docs
+        .map<Product>((doc) => Product(
+            price: doc['price'],
+            productName: doc['name'],
+            category: doc['category'],
+            tag: doc['tag'],
+            seller: "Unknown Seller",
+            url: doc['picture'],
+            rating: doc['rating']))
+        .toList();
+    for (var i = 0; i < r.docs.length; i++) {
+      var r2 = await r.docs[i]["seller"].get();
+      if (r2.data() != null) _productsTemp[i].seller = r2.data()["name"];
+    }
+    setState(() {
+      _products = _productsTemp;
+      _promotions = _products?.where((p) => (p.price < 100)).toList();
+      _recommendations = _products?.where((p) => (p.rating >= 4.5)).toList();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
-    DBService db = DBService();
     return Scaffold(
       appBar: ActionBar(),
-      body: FutureBuilder(
-          future: db.productCollection.get(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (!snapshot.hasData) return const Text("Loading...");
-            var _products = snapshot.data.docs
-                .map<Product>((doc) => Product(
-                    price: doc.data()['price'],
-                    productName: doc.data()['name'],
-                    category: doc.data()['category'],
-                    tag: doc.data()['tag'],
-                    seller: doc.data()['seller'],
-                    url: doc.data()['picture'],
-                    rating: doc.data()['rating']))
-                .toList();
-            final _promotions =
-                _products.where((p) => (p.price < 100) as bool).toList();
-            final _recommendations =
-                _products.where((p) => (p.rating >= 4.5) as bool).toList();
-            return SingleChildScrollView(
+      body: _products == null
+          ? const Text("Loading..")
+          : SingleChildScrollView(
               child: Column(
                 children: [
                   OutlinedButton.icon(
@@ -57,9 +75,9 @@ class _FeedState extends State<Feed> {
                       padding: Dimen.regularPadding,
                       child: Row(
                         children: List.generate(
-                            _promotions.length,
+                            _promotions!.length,
                             (index) => Row(children: [
-                                  productPreview(_promotions[index]),
+                                  productPreview(_promotions![index]),
                                   const SizedBox(width: 8)
                                 ])),
                       ),
@@ -75,9 +93,9 @@ class _FeedState extends State<Feed> {
                       padding: Dimen.regularPadding,
                       child: Row(
                         children: List.generate(
-                            _recommendations.length,
+                            _recommendations!.length,
                             (index) => Row(children: [
-                                  productPreview(_recommendations[index]),
+                                  productPreview(_recommendations![index]),
                                   const SizedBox(width: 8)
                                 ])),
                       ),
@@ -85,8 +103,7 @@ class _FeedState extends State<Feed> {
                   ),
                 ],
               ),
-            );
-          }),
+            ),
     );
   }
 }
